f76ad1ac8782eaa37d6a6771ed1ad4e6906404d3
Johannes Wienke <languitar@semipol.de>
2021-01-03 22:27:44 +0100

    Simplify test code
    Provide a unified fixture for all integration test cases that avoids
    repeated code.

--- test_passgithelper.py
+++ test_passgithelper.py
@@ -1,6 +1,7 @@
 import configparser
+from dataclasses import dataclass
 import io
-from typing import Any, Optional, Sequence, Text
+from typing import Any, Iterable, Optional, Sequence, Text
 
 import pytest
 from pytest_mock import MockFixture
@@ -8,10 +9,31 @@ from pytest_mock import MockFixture
 import passgithelper
 
 
+@dataclass
+class HelperConfig:
+    xdg_dir: Optional[str]
+    request: str
+    entry_data: bytes
+    entry_name: Optional[str] = None
+
+
 @pytest.fixture()
-def _xdg_dir(request: Any, mocker: MockFixture) -> None:
+def _helper_config(mocker: MockFixture, request: Any) -> Iterable[None]:
     xdg_mock = mocker.patch("xdg.BaseDirectory.load_first_config")
-    xdg_mock.return_value = request.param
+    xdg_mock.return_value = request.param.xdg_dir
+
+    mocker.patch("sys.stdin.readlines").return_value = io.StringIO(
+        request.param.request
+    )
+
+    subprocess_mock = mocker.patch("subprocess.check_output")
+    subprocess_mock.return_value = request.param.entry_data
+
+    yield
+
+    if request.param.entry_name is not None:
+        subprocess_mock.assert_called_once()
+        subprocess_mock.assert_called_with(["pass", "show", request.param.entry_name])
 
 
 def test_handle_skip_nothing(monkeypatch: Any) -> None:
@@ -109,22 +131,34 @@ class TestEntryNameExtractor:
 
 
 @pytest.mark.parametrize(
-    "_xdg_dir",
-    [None],
+    "_helper_config",
+    [
+        HelperConfig(
+            None,
+            "",
+            b"ignored",
+        ),
+    ],
     indirect=True,
 )
-@pytest.mark.usefixtures("_xdg_dir")
+@pytest.mark.usefixtures("_helper_config")
 def test_parse_mapping_file_missing() -> None:
     with pytest.raises(RuntimeError):
         passgithelper.parse_mapping(None)
 
 
 @pytest.mark.parametrize(
-    "_xdg_dir",
-    ["test_data/smoke"],
+    "_helper_config",
+    [
+        HelperConfig(
+            "test_data/smoke",
+            "",
+            b"ignored",
+        ),
+    ],
     indirect=True,
 )
-@pytest.mark.usefixtures("_xdg_dir")
+@pytest.mark.usefixtures("_helper_config")
 def test_parse_mapping_from_xdg() -> None:
     config = passgithelper.parse_mapping(None)
     assert "mytest.com" in config
@@ -147,364 +181,272 @@ class TestScript:
         assert not err
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/smoke"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_smoke_resolve(
-        self, monkeypatch: Any, mocker: MockFixture, capsys: Any
-    ) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/smoke",
                 """
 protocol=https
-host=mytest.com"""
+host=mytest.com""",
+                b"narf",
+                "dev/mytest",
             ),
-        )
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        subprocess_mock.return_value = b"narf"
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_smoke_resolve(self, capsys: Any) -> None:
         passgithelper.main(["get"])
-
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(["pass", "show", "dev/mytest"])
 
         out, _ = capsys.readouterr()
         assert out == "password=narf\n"
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/smoke"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_path_used_if_present_fails(self, monkeypatch: Any) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/smoke",
                 """
 protocol=https
 host=mytest.com
-path=/foo/bar.git"""
+path=/foo/bar.git""",
+                b"ignored",
             ),
-        )
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_path_used_if_present_fails(self) -> None:
         with pytest.raises(ValueError, match="No mapping section"):
             passgithelper.main(["get"])
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/with-path"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_path_used_if_present(
-        self, monkeypatch: Any, mocker: MockFixture, capsys: Any
-    ) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/with-path",
                 """
 protocol=https
 host=mytest.com
-path=subpath/bar.git"""
+path=subpath/bar.git""",
+                b"narf",
+                "dev/mytest",
             ),
-        )
-
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        subprocess_mock.return_value = b"narf"
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_path_used_if_present(self, capsys: Any) -> None:
         passgithelper.main(["get"])
-
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(["pass", "show", "dev/mytest"])
 
         out, _ = capsys.readouterr()
         assert out == "password=narf\n"
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/wildcard"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_wildcard_matching(
-        self, monkeypatch: Any, mocker: MockFixture, capsys: Any
-    ) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/wildcard",
                 """
 protocol=https
 host=wildcard.com
 username=wildcard
-path=subpath/bar.git"""
+path=subpath/bar.git""",
+                b"narf-wildcard",
+                "dev/wildcard.com/wildcard",
             ),
-        )
-
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        subprocess_mock.return_value = b"narf-wildcard"
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_wildcard_matching(self, capsys: Any) -> None:
         passgithelper.main(["get"])
-
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(
-            ["pass", "show", "dev/wildcard.com/wildcard"]
-        )
 
         out, _ = capsys.readouterr()
         assert out == "password=narf-wildcard\n"
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/with-username"],
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/with-username",
+                """
+host=plainline.com""",
+                b"password\nusername",
+                "dev/plainline",
+            ),
+        ],
         indirect=True,
     )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_username_provided(
-        self, monkeypatch: Any, mocker: MockFixture, capsys: Any
-    ) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
-                """
-protocol=https
-host=plainline.com"""
-            ),
-        )
-
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        subprocess_mock.return_value = b"password\nusername"
-
+    @pytest.mark.usefixtures("_helper_config")
+    def test_username_provided(self, capsys: Any) -> None:
         passgithelper.main(["get"])
-
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(["pass", "show", "dev/plainline"])
 
         out, _ = capsys.readouterr()
         assert out == "password=password\nusername=username\n"
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/with-username"],
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/with-username",
+                """
+host=plainline.com
+username=narf""",
+                b"password\nusername",
+                "dev/plainline",
+            ),
+        ],
         indirect=True,
     )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_username_skipped_if_provided(
-        self, monkeypatch: Any, mocker: MockFixture, capsys: Any
-    ) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
-                """
-protocol=https
-host=plainline.com
-username=narf"""
-            ),
-        )
-
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        subprocess_mock.return_value = b"password\nusername"
-
+    @pytest.mark.usefixtures("_helper_config")
+    def test_username_skipped_if_provided(self, capsys: Any) -> None:
         passgithelper.main(["get"])
-
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(["pass", "show", "dev/plainline"])
 
         out, _ = capsys.readouterr()
         assert out == "password=password\n"
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/with-username"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_custom_mapping_used(
-        self, monkeypatch: Any, mocker: MockFixture, capsys: Any
-    ) -> None:
-        # this would fail for the default file from with-username
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/with-username",
                 """
 protocol=https
-host=mytest.com"""
+host=mytest.com""",
+                b"narf",
+                "dev/mytest",
             ),
-        )
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        subprocess_mock.return_value = b"narf"
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_custom_mapping_used(self, capsys: Any) -> None:
+        # this would fail for the default file from with-username
         passgithelper.main(["-m", "test_data/smoke/git-pass-mapping.ini", "get"])
-
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(["pass", "show", "dev/mytest"])
 
         out, _ = capsys.readouterr()
         assert out == "password=narf\n"
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/with-username-skip"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_prefix_skipping(
-        self, monkeypatch: Any, mocker: MockFixture, capsys: Any
-    ) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/with-username-skip",
                 """
 protocol=https
-host=mytest.com"""
+host=mytest.com""",
+                b"password: xyz\nuser: tester",
+                "dev/mytest",
             ),
-        )
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        subprocess_mock.return_value = b"password: xyz\nuser: tester"
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_prefix_skipping(self, capsys: Any) -> None:
         passgithelper.main(["get"])
-
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(["pass", "show", "dev/mytest"])
 
         out, _ = capsys.readouterr()
         assert out == "password=xyz\nusername=tester\n"
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/unknown-username-extractor"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_select_unknown_extractor(self, monkeypatch: Any, capsys: Any) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/unknown-username-extractor",
                 """
 protocol=https
-host=mytest.com"""
+host=mytest.com""",
+                b"ignored",
             ),
-        )
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_select_unknown_extractor(self) -> None:
         with pytest.raises(KeyError):
             passgithelper.main(["get"])
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/regex-extraction"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_regex_username_selection(
-        self, monkeypatch: Any, mocker: MockFixture, capsys: Any
-    ) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/regex-extraction",
                 """
 protocol=https
-host=mytest.com"""
+host=mytest.com""",
+                b"xyz\nsomeline\nmyuser: tester\n" b"morestuff\nmyuser: ignore",
+                "dev/mytest",
             ),
-        )
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        subprocess_mock.return_value = (
-            b"xyz\nsomeline\nmyuser: tester\n" b"morestuff\nmyuser: ignore"
-        )
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_regex_username_selection(self, capsys: Any) -> None:
         passgithelper.main(["get"])
-
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(["pass", "show", "dev/mytest"])
 
         out, _ = capsys.readouterr()
         assert out == "password=xyz\nusername=tester\n"
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/entry-name-extraction"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_entry_name_is_user(
-        self, monkeypatch: Any, mocker: MockFixture, capsys: Any
-    ) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/entry-name-extraction",
                 """
 protocol=https
-host=mytest.com"""
+host=mytest.com""",
+                b"xyz",
+                "dev/mytest/myuser",
             ),
-        )
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        subprocess_mock.return_value = b"xyz"
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_entry_name_is_user(self, capsys: Any) -> None:
         passgithelper.main(["get"])
-
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(["pass", "show", "dev/mytest/myuser"])
 
         out, _ = capsys.readouterr()
         assert out == "password=xyz\nusername=myuser\n"
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/with-encoding"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_uses_configured_encoding(
-        self, monkeypatch: Any, mocker: MockFixture, capsys: Any
-    ) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/with-encoding",
                 """
 protocol=https
-host=mytest.com"""
+host=mytest.com""",
+                "täßt".encode("LATIN1"),
+                "dev/mytest",
             ),
-        )
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        password = "täßt"
-        subprocess_mock.return_value = password.encode("LATIN1")
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_uses_configured_encoding(self, capsys: Any) -> None:
         passgithelper.main(["get"])
 
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(["pass", "show", "dev/mytest"])
-
         out, _ = capsys.readouterr()
-        assert out == f"password={password}\n"
+        assert out == f"password=täßt\n"
 
     @pytest.mark.parametrize(
-        "_xdg_dir",
-        ["test_data/smoke"],
-        indirect=True,
-    )
-    @pytest.mark.usefixtures("_xdg_dir")
-    def test_uses_utf8_by_default(
-        self, mocker: MockFixture, monkeypatch: Any, capsys: Any
-    ) -> None:
-        monkeypatch.setattr(
-            "sys.stdin",
-            io.StringIO(
+        "_helper_config",
+        [
+            HelperConfig(
+                "test_data/smoke",
                 """
 protocol=https
-host=mytest.com"""
+host=mytest.com""",
+                "täßt".encode("UTF-8"),
+                "dev/mytest",
             ),
-        )
-
-        subprocess_mock = mocker.patch("subprocess.check_output")
-        password = "täßt"
-        subprocess_mock.return_value = password.encode("UTF-8")
-
+        ],
+        indirect=True,
+    )
+    @pytest.mark.usefixtures("_helper_config")
+    def test_uses_utf8_by_default(self, capsys: Any) -> None:
         passgithelper.main(["get"])
 
-        subprocess_mock.assert_called_once()
-        subprocess_mock.assert_called_with(["pass", "show", "dev/mytest"])
-
         out, _ = capsys.readouterr()
-        assert out == f"password={password}\n"
+        assert out == "password=täßt\n"
