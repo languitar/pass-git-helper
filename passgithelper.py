1bcf288977601d3fa0466e5deb0a35d90d70ca60
Matěj Cepl <mcepl@cepl.eu>
2025-01-22 19:16:35 +0100

    feat(setup): switch from setup.py to setup.cfg
    This is very conservative option, which should work on all normal
    (and even less than normal) Python platforms. However, I found
    declarative syntax to be much more pleasing.
    
    Also update .releaserc.json to update version number in the right file.

--- passgithelper.py
+++ passgithelper.py
@@ -22,6 +22,8 @@ from typing import Dict, IO, Mapping, Optional, Pattern, Sequence, Text
 import xdg.BaseDirectory
 
 
+__version__ = "3.1.0"
+
 LOGGER = logging.getLogger()
 CONFIG_FILE_NAME = "git-pass-mapping.ini"
 DEFAULT_CONFIG_FILE = (
